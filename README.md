378c4cfa6935a2f48a89a970aad4395394c470b7
naeemsulehri <thesuleri@mail.com>
2022-03-08 11:18:02 +0100

    Removed history, due to sensitive data

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProjectMemoryGame
+ React.js Project where complete responsive Memoray Game is developed using material UI and Bootsrta. React Hooks are used to manage the state
---
+++ src/components/Finish/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import './style.css';
+
+const Finish = ({ handleRestart, showModal, bestScore, moves }) => {
+	return (
+		<div>
+			<Dialog
+				open={showModal}
+				disableBackdropClick
+				disableEscapeKeyDown
+				aria-labelledby='alert-dialog-title'
+				aria-describedby='alert-dialog-description'>
+				<h2>Your result's of Memory Game</h2>
+				<DialogTitle id='alert-dialog-title'>
+					Congratulations You have completed the Game !!
+				</DialogTitle>
+				<DialogContent>
+					<DialogContentText
+						id='alert-dialog-description'
+						color='secondary'>
+						Total Moves: {moves} Your best score is: {bestScore}
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleRestart} color='primary'>
+						Play again
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
+	);
+};
+
+export default Finish;
